ee0c6ad67e42e8e14d211b0e01e8a773fdb27c52
lfikre <97213677+lfikre@users.noreply.github.com>
2022-01-26 23:49:05 -0500

    Added prework files to repository

---
+++ Tip calculator/Tip_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_calculatorApp.swift
+//  Tip calculator
+//
+//  Created by leoul fikre on 1/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Tip_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
